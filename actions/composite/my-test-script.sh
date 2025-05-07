aa7ebb0f2710f25d044303559f88d6c7f362a1be
tjololo <1145298+tjololo@users.noreply.github.com>
2025-05-07 20:28:08 +0200

    test upgrading shell script

---
+++ actions/composite/my-test-script.sh
@@ -0,0 +1,4 @@
+# renovate: datasource=github-tags depName=kubectl packageName=kubernetes/kubectl versioning=semver
+KUBECTL_VERSION="v1.32.1"
+# renovate: datasource=github-releases depName=helm packageName=helm/helm versioning=semver
+HELM_VERSION="v3.17.1"
