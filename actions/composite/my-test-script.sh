20f03cabf4e29b2435703b2d0c246eaa31a3f16f
tjololo <1145298+tjololo@users.noreply.github.com>
2025-05-07 20:53:50 +0200

    test removing optional v prefix

--- actions/composite/my-test-script.sh
+++ actions/composite/my-test-script.sh
@@ -1,4 +1,6 @@
-# renovate: datasource=github-tags depName=kubectl packageName=kubernetes/kubectl versioning=semver extractVersion="^kubernetes-(?<version>.*)$"
+# renovate: datasource=github-tags depName=kubectl packageName=kubernetes/kubectl versioning=semver extractVersion="^kubernetes-(?<version>.*)
 KUBECTL_VERSION="v1.32.1"
+# renovate: datasource=deb depName=kubectl packageName=kubectl versioning=semver
+KUBECTL2_VERSION="v1.32.1"
 # renovate: datasource=github-releases depName=helm packageName=helm/helm versioning=semver
 HELM_VERSION="v3.17.1"
